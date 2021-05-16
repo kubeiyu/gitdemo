1cecfe2396a3b4f2d684ea10cd22b319abafcd6c
Kubeiyu <kubeiyu@gmail.com>
2021-05-16 16:08:11 +0800

    Main Commit 2

--- src/main/java/GitDemo.java
+++ src/main/java/GitDemo.java
@@ -7,5 +7,6 @@ public class GitDemo {
     public static void main(String[] args){
         System.out.println("Hello Git");
         System.out.println("Main");
+        System.out.println("2");
     }
 }
